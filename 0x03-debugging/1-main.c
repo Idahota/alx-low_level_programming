2bd045722732e5da098609e83df8eea215c5f247
Idahota <akpasubi_v@yahoo.com>
2022-06-09 11:13:56 -0700

    My first debugging tasts

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+void positive_or_negative(int i);
+/**
+ * * main - tests function that prints if integer is positive or negative
+ * * Return: 0
+ * i#include "main.h"
+ * void positive_or_negati
+ */
