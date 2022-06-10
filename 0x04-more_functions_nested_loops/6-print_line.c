3d3159b37b09777b91e098f5e044eb7bcc0e1dd5
Idahota <akpasubi_v@yahoo.com>
2022-06-09 18:28:48 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  * print_line - a function that draws a straight line in the terminal
+ *  #include "main.h"
+ *
+ */**
+ * print_line - a function that draws a straight line in the terminal
+ *  * @n: input number of times to print '_'
+ *   * Return: a straight line
+ *    */* @n: input number of times to print '_'
+ * Return: a straight line
+ */
