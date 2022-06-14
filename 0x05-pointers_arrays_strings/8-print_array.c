d7a0219229145c3c5768f74d59dc2ca16ec6fc5e
Idahota <akpasubi_v@yahoo.com>
2022-06-13 18:40:21 -0700

    My pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an inputted number of elements
+ * of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+ */
