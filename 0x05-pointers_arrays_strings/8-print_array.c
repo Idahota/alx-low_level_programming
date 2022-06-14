c17a1c7ff9622cb9327c4467edcbcbda272ffcfe
Idahota <105236343+Idahota@users.noreply.github.com>
2022-06-14 02:41:58 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,3 +7,20 @@
  * @a: The array of integers.
  * @n: The number of elements to be printed.
  */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
