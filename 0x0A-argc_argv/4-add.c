91cbbb3644ea86e23d357e6da9f62933bd88fba5
Idahota <105236343+Idahota@users.noreply.github.com>
2022-06-28 03:57:04 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,51 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
-* numchecker - verifies if string has only numbers
-* @s: string to check
-* Description: return 1 if only numbers in string, 0 if not
-* Return: 1 if only numbers, 0 if not
-**/
-
-int numchecker(char *s)
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
+int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-		return (0);
-	}
-	return (1);
-}
+	int num, digit, sum = 0;
 
-/**
-* main - adds positive numbers.
-* @argc: arguments counter.
-* @argv: pointer to array of arguments
-* Return: zero
-**/
-
-int main(int argc, char **argv)
-{
-	int sum, i;
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	for (num = 1; num < argc; num++)
 	{
-		if (numchecker(argv[i]) == 0)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
-		if (atoi(argv[i]) <= 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		sum += atoi(argv[num]);
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
