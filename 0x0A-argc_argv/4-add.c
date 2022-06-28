79f2d0190ec9c01697d1e06f24cebd9d43c6973a
Idahota <105236343+Idahota@users.noreply.github.com>
2022-06-28 01:36:57 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Prints the addition of positive numbers,
- * followed by a new line.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: If one of the numbers contains symbols that are non-digits - 1.
- *  Otherwise - 0.
- */
+* numchecker - verifies if string has only numbers
+* @s: string to check
+* Description: return 1 if only numbers in string, 0 if not
+* Return: 1 if only numbers, 0 if not
+**/
+
+int numchecker(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+		return (0);
+	}
+	return (1);
+}
+
+/**
+* main - adds positive numbers.
+* @argc: arguments counter.
+* @argv: pointer to array of arguments
+* Return: zero
+**/
+
+int main(int argc, char **argv)
+{
+	int sum, i;
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (numchecker(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		if (atoi(argv[i]) <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
