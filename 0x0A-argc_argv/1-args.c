bff79778d8f6b986ee7c8a5a3d6bce2e426d6b23
Idahota <105236343+Idahota@users.noreply.github.com>
2022-06-28 03:46:30 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - prints the number of arguments passed into it
-* @argc: arguments counter.
-* @argv: pointer to array of arguments
-* Return: Returns 0.
-**/
-
-int main(int argc, char *argv[])
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	(void)argv; /*Surpress unused*/
-	printf("%d\n", (argc - 1));
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
