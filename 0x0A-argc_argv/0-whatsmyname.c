3e2cfe536a38d1d23259508795312104bab3f18d
Idahota <105236343+Idahota@users.noreply.github.com>
2022-06-28 03:45:19 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - prints the file/command name
-* @argc: arguments counter.
-* @argv: pointer to array of arguments
-* Return: zero
-**/
-
-int main(int argc, char *argv[])
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the argument.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
-	(void)argc; /*Surpress unused*/
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
