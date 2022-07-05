381c6832247fb744fa9b73e55a9e4a77f43180ad
Idahota <105236343+Idahota@users.noreply.github.com>
2022-07-05 04:25:06 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,6 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
@@ -16,5 +15,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	{
+	}
 }
